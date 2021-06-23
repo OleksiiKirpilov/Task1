283988142427f3747f2e9e995ada678e54288310
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-06-23 23:12:03 +0300

    implemented Part2

--- src/main/java/com/epam/rd/java/basic/practice1/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part2.java
@@ -3,7 +3,11 @@ package com.epam.rd.java.basic.practice1;
 public class Part2 {
 
     public static void main(String[] args) {
-
+        int sum = 0;
+        for(int i = 0; i<2; ++i){
+            sum += Integer.parseInt(args[i]);
+        }
+        System.out.print(sum);		
     }
 	
 }
