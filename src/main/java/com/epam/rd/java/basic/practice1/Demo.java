a3de2004e80d9b0be8dd73c01501446b2fabbdcc
zTmEaNus <ztmeanus@email.com>
2021-06-23 20:26:27 +0000

    Update Demo.java

--- src/main/java/com/epam/rd/java/basic/practice1/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Demo.java
@@ -4,6 +4,7 @@ package com.epam.rd.java.basic.practice1;
 public class Demo {
 
     public static void main(String[] args) {
+        // Not implemented yet
         throw new UnsupportedOperationException("Not implemented.");        
     }
 
