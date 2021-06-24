d12ad209d9160079278c4cc77384359e1cffd210
zTmEaNus <ztmeanus@email.com>
2021-06-24 18:11:05 +0000

    Update Part3.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part3.java
@@ -3,7 +3,7 @@ package com.epam.rd.java.basic.practice1;
 public class Part3 {
 
     public static void main(String[] args) {
-        
+        System.out.print(String.join(" ", args));
     }
 	
 }
