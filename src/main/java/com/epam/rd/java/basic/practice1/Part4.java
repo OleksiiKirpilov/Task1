73dbb3843fdfdbba55e3732793e71fe0d082c4b3
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-06-24 21:30:46 +0300

    4&5

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -3,7 +3,9 @@ package com.epam.rd.java.basic.practice1;
 public class Part4 {
 
     public static void main(String[] args) {
-        
+        int number1 = Integer.parseInt(args[0]);
+        int number2 = Integer.parseInt(args[1]);
+
     }
 
 }
--- src/main/java/com/epam/rd/java/basic/practice1/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part5.java
@@ -3,6 +3,11 @@ package com.epam.rd.java.basic.practice1;
 public class Part5 {
 
     public static void main(String[] args) {
+        int sum = 0;
+        for(char c : args[0].toCharArray()){
+            sum += Integer.parseInt(String.valueOf(c));
+        }
+        System.out.print(sum);
         
     }
 	
