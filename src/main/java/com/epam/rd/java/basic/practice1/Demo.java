f01a8fa2cad0a793d5cc223a6145678fb7b3343e
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-06-24 22:53:14 +0300

    updated Demo

--- src/main/java/com/epam/rd/java/basic/practice1/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Demo.java
@@ -4,7 +4,13 @@ package com.epam.rd.java.basic.practice1;
 public class Demo {
 
     public static void main(String[] args) {
-        Part1.main(new String[0]);        
+        Part1.main(new String[0]);
+        Part2.main({"1", "10", "100"});
+        Part3.main({"param1", "param2", "param3"});
+        Part4.main({"65536", "256"});
+        Part5.main("111");
+        Part6.main("100");
+
     }
 
 }
