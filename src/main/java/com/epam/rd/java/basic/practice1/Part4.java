e27270223867eee1d84855fa9007d16cdcc05aa9
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-06-24 22:31:05 +0300

    implemented Part4

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -5,7 +5,12 @@ public class Part4 {
     public static void main(String[] args) {
         int number1 = Integer.parseInt(args[0]);
         int number2 = Integer.parseInt(args[1]);
+        System.out.print(gcd(number1, number2));
+    }
 
+    static int gcd(int a, int b) {
+        if (b==0) return a;
+        return gcd(b,a%b);
     }
 
 }
