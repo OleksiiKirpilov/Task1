f82fdb7f7fe523ac21aa1df76c08449cfb9f5acb
zTmEaNus <ztmeanus@email.com>
2021-06-24 18:10:43 +0000

    Update Part2.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part2.java
@@ -4,8 +4,8 @@ public class Part2 {
 
     public static void main(String[] args) {
         int sum = 0;
-        for(int i = 0; i<2; ++i){
-            sum += Integer.parseInt(args[i]);
+        for(String arg : args){
+            sum += Integer.parseInt(arg);
         }
         System.out.print(sum);		
     }
