f3e430f57f9153291820216c87e48677b270e6f7
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-06-25 22:59:20 +0300

    updated Part7

--- src/main/java/com/epam/rd/java/basic/practice1/Part7.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part7.java
@@ -5,15 +5,35 @@ public class Part7 {
     static final int RADIX = 26;
 
     public static void main(String[] args) {
-    	System.out.print(rightColumn("AA"));
+        System.out.println(str2int("A")); 
+		System.out.println(str2int("Z"));
+        System.out.println(str2int("AA"));
+		System.out.println(str2int("AAA"));
+		System.out.println(int2str(2));		
+		System.out.println(int2str(26));
+		System.out.println(int2str(27));
+		System.out.println(int2str(703));
     }
 
     public static int str2int(String number) {
-        return Integer.parseInt(number, RADIX);
+        
+		int n = 0;
+		for(char c : number.toCharArray()){
+			n += (c - 'A' + 1);
+			n *= RADIX;
+		}
+		return n/RADIX;
+
     }
 
     public static String int2str(int number) {
-        return Integer.toString(number, RADIX);
+        String result = "";
+		while(number > 0){
+			char c = (char) ('A' + ((number - 1) % RADIX));
+			number = (number - 1) / RADIX;
+			result = String.valueOf(c) + result;
+		}
+        return result;
     }
 
     public static String rightColumn(String number) {
