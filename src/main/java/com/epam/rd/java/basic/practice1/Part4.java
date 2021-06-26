27138c82da3f1b95f1f85b511e5f1631a86a7825
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-06-26 19:45:18 +0300

    updated Part4

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -9,8 +9,12 @@ public class Part4 {
     }
 
     static int gcd(int a, int b) {
-        if (b==0) return a;
-        return gcd(b,a%b);
+        int t;
+        while(b != 0){
+            t = a;
+            a = b;
+            b = t % b;
+        }
+        return a;
     }
-
 }
--- src/run_Part7.bat
+++
@@ -1,3 +0,0 @@
-@pushd main\java
-javac com\epam\rd\java\basic\practice1\Part7.java && java com.epam.rd.java.basic.practice1.Part7
-@popd
