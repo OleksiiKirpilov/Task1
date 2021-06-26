1b169a0c7e35ef1449c522431e4ac4e76b4a3cd9
zTmEaNus <ztmeanus@email.com>
2021-06-26 04:29:39 +0000

    Update Part4.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -9,8 +9,8 @@ public class Part4 {
     }
 
     static int gcd(int a, int b) {
-        if (b==0) return a;
-        return gcd(b,a%b);
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 
 }
